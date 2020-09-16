fabd394c476de8e76c43a6f2dc6bf364c64625a5
sageti <sagetindeland@gmail.com>
2020-09-16 22:46:41 +0200

    Create script.js
    Answers added to questin 1-8

---
+++ script.js
@@ -0,0 +1,36 @@
+//Module Assignment 1 - Level 1
+
+//question 1 - Declare a variable called firstVar but don't initialise it with a value.
+
+var firstVar;
+
+//question 2 - Declare a variable called name and assign it your first name.
+
+var name = "Sage";
+
+//question 3 - Declare and initialise a variable with a number value.
+
+var number = 1;
+
+//question 4 - Create a variable called division and initialise it with a value of 20 divided by 5.
+
+var division = 20 / 5;
+
+//question 5 - Write code that checks the type of the value "frog".
+
+typeof "frog";
+
+//question 6 - Declare and initialise a variable called orderHasShipped with a boolean value.
+var orderHasShipped = true;
+
+//question 7 - Create an if statement that checks if orderHasShipped is true. If it is true, console log the string value "true". If not, console log the string value "false".
+if (orderHasShipped === true) {
+	console.log("true");
+} else {
+	console.log("false");
+}
+
+//question 8 - Create a for loop that counts from 0 to 9. Console log the value of the counter variable inside the loop.
+for (var i = 0; i <= 9; i++) {
+	console.log(i);
+}
